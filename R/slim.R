eb2b0b012fe0e6e0eee1d3bfe280079a255cc5c9
Martin Petr <git@bodkan.net>
2021-03-05 12:15:55 +0100

    Add run_slimgui()

--- R/slim.R
+++ R/slim.R
@@ -1,5 +1,8 @@
-run_slimgui <- function(path = NULL) {
-  if (!is.null(path)) {
-    system("/Applications/SLiMgui.app/Contents/MacOS/SLiMgui")
-  }
+#' Open the SLiM backend script in the SLiM gui
+#'
+#' @export
+run_slimgui <- function() {
+  # backend_script <- system.file("backend.slim", package = "spammr")
+  backend_script <- "~/projects/spammr/inst/extdata/backend.slim"
+  system(sprintf("open -a SLiMgui %s", backend_script))
 }
