0e74122bb0e9bee4c0eb8fe89799ae7514be9d5e
Martin Petr <git@bodkan.net>
2021-03-05 16:29:07 +0100

    Process compiled raster maps to a serializable form

--- R/compilation.R
+++ R/compilation.R
@@ -6,13 +6,12 @@
 #'
 #' @export
 compile <- function(..., dry_run = T) {
-#  pops <- list(...)
-  pops <- list(ooa, whg, yam, ana, neol)
+  pops <- list(...)
 
   # iterate over all spammr population objects and extract
   # information about population split hierarchy and split times
   # into a data frame
-  splits <- lapply(pops, function(p) {
+  splits_table <- lapply(pops, function(p) {
     parent <- attr(p, "parent")
    if (is.character(parent) && parent == "ancestor") {
      parent_name <- parent
@@ -29,20 +28,28 @@ compile <- function(..., dry_run = T) {
   }) %>% do.call(rbind, .)
 
   # order populations by split time and assign a numberic identifier to each
-  splits <- splits[order(splits$time, decreasing = TRUE), ]
-  splits$pop_id <- 1:nrow(splits) - 1
-  splits$parent_id <- lapply(
-    splits$parent,
-    function(i) splits[splits$pop == i, ]$pop_id
+  splits_table <- splits_table[order(splits_table$time, decreasing = TRUE), ]
+  splits_table$pop_id <- 1:nrow(splits_table) - 1
+  splits_table$parent_id <- lapply(
+    splits_table$parent,
+    function(i) splits_table[splits_table$pop == i, ]$pop_id
   ) %>% unlist() %>% c(-1, .)
 
   # generate spatial bit maps
   maps <- render(..., intersect = T)
 
+  # convert list of rasters into data frame, adding the spatial
+  # maps themselves as a list column
+  maps_table <- lapply(maps, function(m) {
+    as.data.frame(m[c("pop_name", "time")])
+  }) %>%
+    do.call(rbind, .)
+  maps_table$map <- I(lapply(maps, function(m) m$map))
+
   if (dry_run) {
     return(list(
-      splits = splits,
-      maps = maps
+      splits = splits_table,
+      maps = maps_table
     ))
   }
 }
@@ -59,8 +66,8 @@ compile <- function(..., dry_run = T) {
 render <- function(..., intersect = TRUE) {
   pops <- list(...)
   raster_list <- lapply(pops, function(pop) {
-    times <- unique(pop$time)
-    snapshots <- lapply(times, function(t) {
+    # iterate over temporal maps for the current population
+    snapshots <- lapply(unique(pop$time), function(t) {
       snapshot <- pop[pop$time == t, ]
       class(snapshot) <- set_class(snapshot, "pop")
 
@@ -68,14 +75,23 @@ render <- function(..., intersect = TRUE) {
       if (is.null(attr(pop, "intersected")) & intersect)
         snapshot <- intersect_features(snapshot)
 
+      # plot the spatial map, using the 'world' context as a bounding box
       bbox <- sf::st_bbox(attr(snapshot, "world"))
-      ggplot() +
+      p_map <- ggplot() +
         geom_sf(data = snapshot, fill = "white", color = NA) +
         coord_sf(xlim = bbox[c(1, 3)], ylim = bbox[c(2, 4)], expand = FALSE) +
         theme_void() +
         theme(plot.background = element_rect(fill = "black"))
+
+      # return the rendered spatial map with the population name and the
+      # appropriate time stamp (unique-ing because intersecting splits
+      # the spatial object into multiple disjoint features)
+      list(
+        pop_name = unique(snapshot$pop),
+        time = unique(snapshot$time),
+        map = p_map
+      )
     })
-    names(snapshots) <- paste(pop$pop, times, sep = "_")
     snapshots
   })
 
