ac797c163555cf5c29400fa490b3edb5baa0a43b
Martin Petr <git@bodkan.net>
2021-03-06 19:24:38 +0100

    Move run SLiM function

--- R/slim.R
+++
@@ -1,8 +0,0 @@
-#' Open the SLiM backend script in the SLiM gui
-#'
-#' @export
-run_slimgui <- function() {
-  # backend_script <- system.file("backend.slim", package = "spammr")
-  backend_script <- "~/projects/spammr/inst/extdata/backend.slim"
-  system(sprintf("open -a SLiMgui %s", backend_script))
-}
