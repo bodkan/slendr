7cb1ce52ca4eb83615667113813f39d269a98985
Martin Petr <git@bodkan.net>
2021-03-13 22:43:38 +0100

    Update the tutorial

--- R/viz.R
+++ R/viz.R
@@ -60,3 +60,41 @@ animate <- function(locations, gif = NULL, gen_time = NULL, nframes) {
   else
     gganimate::anim_save(gif, anim)
 }
+
+
+plot_model <- function(pops, admixtures, time = -Inf) {
+  pops <- pops[tsplit >= time | is.na(tsplit)]
+  admixtures <- admixtures[tstart >= time]
+  ggplot(pops, aes(x + 100, y + 50)) +
+    background_image(map) +
+    # plot directions of population splits
+    geom_segment(aes(x = xfrom, y = yfrom, xend = x, yend = y, color = name),
+                 data = na.omit(pops), size = 1,
+                 arrow = arrow(length = unit(0.5, "cm"), type = "closed")) +
+    # plot directions of admixture events
+    geom_segment(
+      aes(x = xfrom, y = yfrom, xend = xto, yend = yto, color = toname),
+      data = admixtures, size = 1,
+      arrow = arrow(length = unit(0.5, "cm"), type = "closed")
+    ) +
+    geom_label(
+      aes(label = sprintf("%s->%s\n%dy-%dy ago\n%.1f%%",
+                          fromname, toname, tstart, tend, rate / 100),
+          x = (xfrom + xto) / 2, y = (yfrom + yto) / 2, fill = fromname),
+      data = admixtures, size = 3
+    ) +
+    geom_point(aes(x, y, color = name), size = 2) +
+    geom_label(aes(label = sprintf("%s (%s)\n%sy ago\nfrom: %s",
+                                   name, pop,
+                                   ifelse(!is.na(tsplit), tsplit, "-"),
+                                   ifelse(!is.na(parentname), parentname, "-")),
+                   fill = name), size = 3) +
+    coord_fixed(xlim = c(0, dim(map)[2]), ylim = c(0, dim(map)[1])) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_color_discrete(drop = FALSE) +
+    scale_fill_discrete(drop = FALSE) +
+    theme(legend.position = "none",
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank())
+}
