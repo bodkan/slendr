786e85463abfa118658fc34ded608b6cfc749a7f
Martin Petr <git@bodkan.net>
2021-04-16 18:39:35 +0200

    Add (lat, lon) -> (pixel x, pixel y) conversion

--- vignettes/rasa.R
+++ vignettes/rasa.R
@@ -1,23 +1,30 @@
-devtools::load_all("~/projects/spammr")
+# always a good idea to first fetch the very latest dev version of the package
+# because the software is updated every couple of hours
+devtools::install_github("bodkan/spammr")
+
+library(spammr)
 
 world <- map(
-  xrange = c(-15, 70),  # min-max longitude
-  yrange = c(20, 75),   # min-max latitude
-  crs = "EPSG:3035"  # real projected CRS used internally
+  xrange = c(-15, 70), # min-max longitude
+  yrange = c(20, 75),  # min-max latitude
+  crs = "EPSG:3035"    # real projected CRS used internally
 )
 
+# restrict individual movement only to this regions
 eurasia <- region("Eurasia", world, coords = list(
   c(-8, 35), c(-5, 36), c(10, 38), c(20, 35), c(35, 30),
   c(60, 30), c(70, 40), c(80, 50), c(50, 70), c(40, 70),
   c(0, 70), c(-15, 50)
 ))
 
+# create a single population
 pop <- population(
   "pop", parent = "ancestor", Ne = 10000,
   world = world, region = eurasia
 )
 plot(pop)
 
+# compile the model specification and the single spatial map
 model <- compile(
   pop,
   model_dir = "~/Desktop/europe/",
@@ -26,45 +33,32 @@ model <- compile(
   overwrite = TRUE
 )
 
+# neutral model for testing the uniformity of the spread
+#   - max_distance is the parameter used in:
+#     initializeInteractionType(1, "xy", reciprocal=T, maxDistance=<<here>>)
+#     calls and is discussed in the SLiM manual (it influences the maximum
+#     spatial competition distance and mating choice distance between individuals)
+#   - max_spread
+# both parameters in units of the raster size (i.e. pixels, same as SLiM)
 run(
-  model,
-  burnin = 100, sim_length = 30000,
+  model, burnin = 30, sim_length = 30000,
   recomb_rate = 0, seq_length = 100,
   max_distance = 50, max_spread = 5
 )
 
+# substitute parameters into the selection script template
+selection_script <- script(
+  system.file("extdata", "selection.slim", package = "spammr"),
+  s = 0.5, # selection coefficient of an additive
+  freq = 0.1, onset = 29000,
+  origin = "pop",
+  coord = convert(lat = 10, lon = 45, model)
+)
 
-
-
-# optional scripts?
-
-# direct path
-script1 <- "~/my/path/to/a/custom/selection/script.slim"
-
+# run the demographic model, including the selection component
 run(
-  model,
-  burnin = 100, sim_length = 30000,
+  model, burnin = 30, sim_length = 30000,
   recomb_rate = 0, seq_length = 100,
   max_distance = 50, max_spread = 5,
-  include = "~/projects/spammr/inst/extdata/selection.slim"
-)
-
-# script template
-script2 <- script(
-  "~/my/path/to/a/custom/selection/script.slim",
-  sel_coef = 0.5, dominance = 0.5,
-  init_freq = 0.1, time = 10,
-  origin = "pop",
-  coord = coordinate(lat = 10, lon = 45, world)
-)
-
-script3 <- script("~/custom/script/for/output/logging.slim", start = 30000, end = 0)
-
-# include in the run
-run(
-  model,
-  burnin = 100, seq_length = 3000,
-  recomb_rate = 0, sim_length = 100,
-  max_distance = 50, max_spread = 5,
-  include = c(script1, script2, script3)
+  include = selection_script
 )
