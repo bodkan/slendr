b3ea4471062f6d4d053241469966de46a1d6afe3
Martin Petr <git@bodkan.net>
2021-04-28 12:51:20 +0200

    Clean up the demo script

--- vignettes/demo.R
+++ vignettes/demo.R
@@ -1,206 +1,80 @@
 devtools::load_all(".")
 
-
-
-
-
-# Define the world map
-
-world <- map(
-  xrange = c(-15, 60),
-  yrange = c(20, 65),
-  crs = "EPSG:3035"
-)
-
-plot(world, title = "Zoomed-in world map")
-
-world
-
-
-
-
-
-# Defining smaller geographic regions
+world <- map(xrange = c(-15, 60), yrange = c(20, 65), crs = "EPSG:3035")
 
 africa <- region(
   "Africa", world,
-  coords = list(
-    c(-18, 20), c(40, 20), c(30, 33),
-    c(20, 32), c(10, 35), c(-8, 35)
-  )
+  coords = list(c(-18, 20), c(40, 20), c(30, 33),
+                c(20, 32), c(10, 35), c(-8, 35))
 )
 
 europe_anatolia <- region(
-  "Western Europe & Anatolia",
-  world,
-  coords = list(
-    c(-10, 35), c(-5, 35), c(10, 38), c(20, 35), c(38, 35),
-    c(40, 40), c(30, 45), c(20, 58), c(-5, 60), c(-15, 50)
-  )
+  "Western Europe & Anatolia", world,
+  coords = list(c(-10, 35), c(-5, 35), c(10, 38), c(20, 35), c(38, 35),
+                c(40, 40), c(30, 45), c(20, 58), c(-5, 60), c(-15, 50))
 )
 
-europe <- region(
-  "Western Europe",
-  world,
-  coords = list(
-    c(-8, 35), c(-5, 36), c(10, 38), c(20, 35), c(25, 35),
-    c(28, 45), c(20, 58), c(-5, 60), c(-15, 50)
-  )
+europe <- region("Western Europe", world,
+  coords = list(c(-8, 35), c(-5, 36), c(10, 38), c(20, 35), c(25, 35),
+                c(28, 45), c(20, 58), c(-5, 60), c(-15, 50))
 )
 
 anatolia <- region(
-  "Anatolia",
-  world,
-  coords = list(
-    c(28, 35), c(40, 35),
-    c(42, 40), c(30, 43), c(27, 40), c(25, 38)
-  )
+  "Anatolia", world,
+  coords = list(c(28, 35), c(40, 35), c(42, 40),
+                c(30, 43), c(27, 40), c(25, 38))
 )
 
-plot(europe, anatolia, title = "Geographic regions")
-
-
-
-
-
-
-## Defining spatial population boundaries
-
-afr <- population(
-  "AFR", parent = "ancestor", N = 2000, 
-  world = world, region = africa
-)
-plot(afr)
+afr <- population("AFR", parent = "ancestor", N = 2000, world = world, region = africa)
 
 ooa <- population(
-  "OOA", parent = afr, time = 51000, N = 200,
-  center = c(33, 30), # (lon, lat)
-  radius = 500, # km
-  remove = 27000 # remove from the sim "27k years ago" of sim time
-)
-
-plot(ooa, intersect = F, title = "'Raw' population range")
-plot(ooa, title = "'Intersected' population range")
-
-# Moving a population
-
-ooa <- ooa %>% move(
+  "OOA", parent = afr, time = 51000, N = 200, remove = 27000,
+  center = c(33, 30), radius = 500
+) %>% move(
   trajectory = list(c(40, 30), c(50, 30), c(60, 40), c(70, 40)),
-  start = 50000,
-  end = 40000,
-  snapshots = 30 # is really necessary to force the user to specify this?
+  start = 50000, end = 40000, snapshots = 30
 )
-plot(ooa, title = "Intermediate migration maps")
-
-
 
 ehg <- population(
-  "EHG", time = 28000, N = 400, parent = ooa,
-  world, coords = list(
-    c(26, 55), c(38, 53), c(48, 53), c(60, 53),
-    c(60, 60), c(48, 63), c(38, 63), c(26, 60)
-  ),
-  remove = 6000
+  "EHG", time = 28000, N = 400, parent = ooa, remove = 6000,
+  world, coords = list(c(26, 55), c(38, 53), c(48, 53), c(60, 53),
+                       c(60, 60), c(48, 63), c(38, 63), c(26, 60))
 )
-plot(ehg)
 
-eur <- population(
-  name = "EUR", time = 25000, N = 1000, parent = ehg,
-  world, region = europe
-)
-plot(eur)
-
-## Spatial population expansion
+eur <- population(name = "EUR", time = 25000, N = 1000, parent = ehg, world, region = europe)
 
 ana <- population(
-  name = "ANA", time = 28000, N = 800, parent = ooa,
-  world, center = c(34, 38), radius = 700,
-  region = anatolia, remove = 6000
-)
-plot(ana)
-
-ana <- ana %>% expand(
-  by = 2500, # km
-  start = 10000, # start when? (thousand years ago)
-  end = 7000, # end when? (thousand years ago)
-  snapshots = 10, # how many spatial snapshots to partition into? (unnecessary?)
-  region = europe_anatolia # restrict expansion into...
-)
-plot(ana)
-
-plot(ana, time_facets = T)
+  name = "ANA", time = 28000, N = 800, parent = ooa, remove = 6000,
+  world, center = c(34, 38), radius = 700, region = anatolia
+) %>% expand(
+  by = 2500, start = 10000, end = 7000, snapshots = 10, region = europe_anatolia)
 
 yam <- population(
-  name = "YAM", time = 7000, N = 600, parent = ehg,
-  world, coords = list(
-    c(26, 50), c(38, 49), c(48, 50),
-    c(48, 56), c(38, 59), c(26, 56)
-  ),
-  remove = 2000
+  name = "YAM", time = 7000, N = 600, parent = ehg, remove = 2000
+  world, coords = list(c(26, 50), c(38, 49), c(48, 50),
+                       c(48, 56), c(38, 59), c(26, 56))
 )
-plot(yam)
 
 yam_migr <- population(
-  name = "YAM_migr", time = 6000, N = 1000, parent = yam,
-  world, coords = list(
-    c(26, 50), c(38, 49), c(48, 50),
-    c(48, 56), c(38, 59), c(26, 56)
-  ),
-  remove = 2900
+  name = "YAM_migr", time = 6000, N = 1000, parent = yam, remove = 2900,
+  world, coords = list(c(26, 50), c(38, 49), c(48, 50),
+                       c(48, 56), c(38, 59), c(26, 56)
 ) %>%
-  move(
-    trajectory = c(15, 50),
-    start = 5000,
-    end = 3000,
-    snapshots = 8
-  )
-plot(yam_migr)
-
-
-
-
-
-# Combined view of all spatial maps
-
-plot(afr, ooa, ehg, eur, ana, yam, yam_migr)
-
-
-
-
-
-
-
-## Define admixture events
+  move(trajectory = c(15, 50), start = 5000, end = 3000, snapshots = 8)
 
 admixtures <- list(
   admixture(from = ana, to = eur, rate = 0.5, start = 8000, end = 6000),
   admixture(from = yam_migr, to = eur, rate = 0.75, start = 4000, end = 3000)
 )
 
-admixtures
-
-
-## Compile the whole model to a format which can be loaded in SLiM
 
 model <- compile(
   populations = list(afr, ooa, ehg, eur, ana, yam, yam_migr),
   admixtures = admixtures,
-  model_dir = "~/Desktop/demo-model",
-  gen_time = 30,
-  resolution = 10, # km per pixel
-  overwrite = TRUE
+  model_dir = "~/Desktop/demo-model", gen_time = 30, resolution = 10
 )
 
-model
-
-
-## Visualize the entire history of splits and admixtures
-
 graph(model)
-
-
-
-## Running the simulation (from a backend SLiM script)
 
 run(
   model, sim_length = 52000,
@@ -211,10 +85,4 @@ run(
 )
 
 
-
-
-## Post-simulation diagnostics
-
-# ancestries(model)
-
-# animate(model, nframes = 200)
+animate(model, nframes = 200)
