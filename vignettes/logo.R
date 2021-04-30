2ae1ab1f1bf40360d92b70c09fc871fcac95c332
Martin Petr <git@bodkan.net>
2021-04-30 17:52:17 +0200

    Add draft logo

---
+++ vignettes/logo.R
@@ -0,0 +1,70 @@
+library(sf)
+library(tidyverse)
+library(rnaturalearth)
+library(hexSticker)
+
+map <- ne_load(scale = 110, type = "land", category = "physical", destdir = "~/Google/postdoc/data/ne_data", returnclass = "sf")
+map <- st_crop(map, st_bbox(c(xmin = -28, xmax = 80, ymin = -49, ymax = 70), crs = 4326))
+
+points_df <- tribble(
+  ~lon, ~lat, ~id,
+  -5, 20, 1,
+  10, 10, 2,
+  20, 25, 3,
+  20, -7, 4,
+  25, 5, 5,
+  35, 10, 6,
+  35, -7, 7,
+  40, 37, 8,
+  25, 40, 9,
+  12, 48, 10,
+  48, 48, 11,
+  62, 35, 12,
+  45, 30, 13,
+  55, 20, 14,
+  45, 17, 15
+) %>% mutate(id = factor(id))
+
+edges_df <- tribble(
+  ~from, ~to,
+  1, 2,
+  1, 3,
+  2, 4,
+  2, 5,
+  3, 6,
+  3, 8,
+  6, 15,
+  5, 6,
+  8, 9,
+  9, 10,
+  8, 11,
+  15, 13,
+  13, 12,
+  15, 14,
+  5, 7
+) %>% mutate(
+  x = sapply(from, function(i) filter(points_df, id == i)$lon),
+  y = sapply(from, function(i) filter(points_df, id == i)$lat),
+  xend = sapply(to, function(i) filter(points_df, id == i)$lon),
+  yend = sapply(to, function(i) filter(points_df, id == i)$lat),
+  from = factor(from), to = factor(to)
+)
+
+p <- ggplot() +
+  geom_sf(data = map, color = NA) +
+#  geom_label(data = points_df, aes(lon, lat, label = id)) +
+  geom_segment(data = edges_df, aes(x = x, y = y, xend = xend, yend = yend, color = from)) +
+  geom_point(data = points_df, aes(lon, lat, color = id), size = 1.5) +
+  guides(color = F, fill = F) +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0)) +
+  theme_void() +
+  theme_transparent() +
+  geom_rect(aes(xmin = -28, xmax = 80, ymin = -32, ymax = -10),
+            fill = "lightblue", alpha = 0.8)
+p
+s <- sticker(p, package="stamp(s)", p_size = 8, p_y = 0.55, p_color = "black",
+        h_color = "black", h_fill = "#0077be",
+        s_y = 1.05, s_x = 1, s_width = 2, s_height = 2,
+        white_around_sticker = T, filename = "~/Desktop/logo.pdf")
+plot(s)
