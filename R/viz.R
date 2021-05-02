e6f9137105abf18783684ec5ec335e37e71e6d84
Martin Petr <git@bodkan.net>
2021-05-02 16:44:48 +0200

    Re-introduce changes after package renaming

--- R/viz.R
+++ R/viz.R
@@ -172,7 +172,7 @@ graph <- function(model) {
     geom_edge_link(aes(filter = type == "intermediate",
                        linetype = "continuation")) +
 
-    # population removal time spannr
+    # population removal time stamp
     geom_edge_link(aes(filter = type == "terminal"), alpha = 0.25) +
 
     geom_node_label(aes(filter = type == "split", fill = pop,
