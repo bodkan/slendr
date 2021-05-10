318d4c9983649a0fe44c862601ea662dba59527f
Martin Petr <git@bodkan.net>
2021-05-10 21:08:05 +0200

    Finalize draft of a shiny-based interactive visualization

--- R/shiny.R
+++ R/shiny.R
@@ -1,58 +1,98 @@
-library(shiny)
+#' Take a list of spannr_pop population boundary objects and
+#' "interpolate" all of them at time points specified by others
+#' (unless a given population is supposed to be removed at that time)
+fill_maps <- function(pops) {
+  
+  removal_times <- sapply(pops, attr, "remove")
 
-# Define UI for application that plots random distributions 
-shinyUI(pageWithSidebar(
+  # get times of all spatial maps across all populations
+  all_times <- unique(sort(c(
+    0,
+    removal_times,
+    unlist(sapply(pops, function(i) i$time))
+  ))) %>% .[. != Inf & . != -1]
+  
+  all_maps <- lapply(seq_along(pops), function(i) {
 
-  # Application title
-  headerPanel("Spatial model population dynamics"),
+    # get times where the spatial map of the current population
+    # needs to be filled in
+    missing_times <- all_times[
+      all_times >= removal_times[i] &
+        !all_times %in% pops[[i]]$time
+    ]
 
-  # Sidebar with a slider input for number of observations
-  sidebarPanel(
-    sliderInput("time_snapshot", 
-                "Time snapshot:", 
-                min = 0,
-                max = 50000, 
-                value = 50000)
-  ),
+    # generate the missing maps
+    new_maps <- lapply(missing_times, function(t) {
+      # get all preceding maps
+      previous_map <- pops[[i]] %>% .[.$time > t, ]
+      if (!nrow(previous_map)) return(NULL)
+      latest_map <- previous_map[nrow(previous_map), ]
+      latest_map$time <- t
+      latest_map
+    }) %>%
+      do.call(rbind, .)
 
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput(outputId = "spannr_map")
-  )
-))
+    if (!is.null(new_maps)) {
+      combined_maps <-
+        rbind(pops[[i]], new_maps) %>%
+        .[order(-.$time), ] %>%
+        .[.$time != Inf, ]
 
-# Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output) {
+      attributes(combined_maps) <- attributes(pops[[i]])
+    } else {
+      combined_maps <- pops[[i]]
+    }
 
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
-  output$spannr_map <- renderPlot({
-    shiny_plot(pops = list(afr, ooa, ehg, ana, eur, yam, yam_migr), input$time_snapshot)
+    combined_maps
+
   })
-})
+
+  all_maps
+}
 
 
-
-#' Render the spatial dynamics as an interactive shiny app
-plot_snapshot <- function(..., time_snapshot, graticules = "original", intersect = TRUE) {
+#' Plot spatial maps
+#'
+#' @import ggplot2
+plot_maps <- function(..., time = NULL, graticules = "original", intersect = TRUE) {
   if (!graticules %in% c("internal", "original"))
     stop("Graticules can be either 'original' or 'internal'", call. = FALSE)
 
-#  pops <- list(...)
-  names(pops) <- sapply(pops, function(i) i$pop[1])
-
+  spatial_maps <- list(...)
+    
   # extract the map component underlying each population object
   # and make sure they are all the same with no conflicts
-  maps <- unique(lapply(pops, function(i) attr(i, "map")))
+  maps <- unique(lapply(spatial_maps, function(i) attr(i, "map")))
   if (length(maps) != 1) {
     stop("Objects do not share the same map component", call. = F)
   }
   map <- maps[[1]]
+  
+  # get all time points defined by the user
+  all_times <- sort(unique(unlist(lapply(spatial_maps, `[[`, "time"))))
+
+  # get split and removal times of all specified populations
+  split_times <- sapply(sapply(spatial_maps, `[[`, "time"), `[`, 1)
+  removal_times <- sapply(spatial_maps, attr, "remove")
+  
+  # if the user specified a time point, "interpolate" all maps at that time
+  # and return just those that match that time point
+  if (!is.null(time)) {
+    spatial_maps <- fill_maps(spatial_maps)
+
+    previous_time <- min(all_times[all_times >= time])
+    # get only those populations already/still present at the
+    # specified time...
+    present_pops <- spatial_maps[split_times >= time & removal_times <= time]
+    # ... and extract their spatial maps
+    spatial_maps <- lapply(present_pops, function(pop) {
+      snapshot <- pop[pop$time == previous_time, ]
+      attributes(snapshot) <- attributes(pop)
+      snapshot
+    })
+  }
+  
+  spatial_maps <- do.call(rbind, lapply(spatial_maps, intersect_features))
 
   # plot the world map (if a real geographic map was specified)
   if (nrow(map)) {
@@ -78,69 +118,86 @@ plot_snapshot <- function(..., time_snapshot, graticules = "original", intersect
     )
   }
 
-  removal_times <- sapply(names(pops), function(pop_name) {
-    pop_maps <- pops[[pop_name]]
-    attr(pops[[pop_name]], "remove")
-  })
-
-  # get times of all spatial maps across all populations
-  all_times <- unique(sort(c(
-    0,
-    removal_times,
-    unlist(sapply(pops, function(i) i$time))
-  ))) %>% .[. != Inf & . != -1]
-
-  all_maps <- lapply(names(pops), function(pop_name) {
-
-    pop_maps <- pops[[pop_name]]
-
-    # get times where the spatial map of the current population
-    # needs to be filled in
-    missing_times <- all_times[
-      all_times >= removal_times[pop_name] &
-        !all_times %in% pop_maps$time
-    ]
-
-    # generate the missing maps
-    new_maps <- lapply(missing_times, function(t) {
-      # get all preceding maps
-      previous_maps <- pop_maps %>% .[.$time > t, ] # what's going on here with 2 maps?
-      if (!nrow(previous_maps)) return(NULL)
-      latest_map <- previous_maps[nrow(previous_maps), ]
-      latest_map$time <- t
-      latest_map
-    }) %>%
-      do.call(rbind, .)
-
-    combined_maps <-
-      rbind(pop_maps, new_maps) %>%
-      .[order(-.$time), ] %>%
-      .[.$time != Inf, ]
-
-    #  attributes(combined_maps) <- attributes(pop_maps)
-
-    combined_maps
-
-  }) %>%
-  do.call(rbind, .) %>%
-  .[order(-.$time), ]
-
-  snapshot <- all_maps[all_maps$time == time_snapshot, ]
-  p_map <-  ggplot() + theme_bw()
-
-if (nrow(map)) {
-  p_map <- geom_sf(data = map,
-                   aes(frame = NULL), fill = "lightgray", color = NA)
-} else {
-  p_map <- NULL
+  if (nrow(map)) {
+    p_map <- geom_sf(data = map,
+                     aes(frame = NULL), fill = "lightgray", color = NA)
+  } else {
+    p_map <- NULL
+  }
+  
+  ggplot() +
+    p_map +
+    geom_sf(data = spatial_maps,
+            aes(fill = pop), color = NA) +
+    scale_fill_discrete(drop = FALSE) +
+    scale_alpha(range = c(1, 0.1)) +
+    guides(fill = FALSE, alpha = guide_legend("time")) +
+    theme_bw() +
+    coord_sf(crs = sf::st_crs(map), datum = graticule_crs, expand = 0)
 }
 
-ggplot() +
-  p_map +
-  geom_sf(data = snapshot,
-          aes(fill = pop, alpha = -time), color = NA) +
-  scale_fill_discrete(drop = FALSE) +
-  scale_alpha(range = c(1, 0.1)) +
-  guides(fill = FALSE, alpha = guide_legend("time"))
 
+#' Open an interactive browser of the spatial model
+#'
+#' @import shiny
+interact <- function(...) {
+  
+  times <- sort(c(0, unique(unlist(lapply(list(...), `[[`, "time")))))
+  
+  # Define UI for app that draws a histogram ----
+  ui <- fluidPage(
+
+    # App title ----
+    titlePanel("Spatial population dynamics"),
+
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+
+      # Sidebar panel for inputs ----
+      sidebarPanel(
+
+        # Input: Slider for the number of bins ----
+        ## sliderInput(
+        ##             min = min(times[times < Inf]),
+        ##             max = max(times[times < Inf]),
+        ##             value = min(times))
+        shinyWidgets::sliderTextInput(
+          inputId = "time_point",
+          label = "Time point:",
+          choices = times[times < Inf],
+          selected = max(times[times < Inf]),
+          width = "100%"
+        )
+
+      ),
+
+      # Main panel for displaying outputs ----
+      mainPanel(
+
+        # Output: Histogram ----
+        plotOutput(outputId = "spannr_shiny")
+
+      )
+    )
+  )
+
+  
+  # Define server logic required to draw a histogram ----
+  server <- function(input, output) {
+    # This expression that generates the figure is wrapped in a call
+    # to renderPlot to indicate that:
+    #
+    # 1. It is "reactive" and therefore should be automatically
+    #    re-executed when inputs (input$bins) change
+    # 2. Its output type is a plot
+    output$spannr_shiny <- renderPlot({
+      
+      # get the last time snapshot before the specified time
+      do.call("plot_maps", c(list(...), time = input$time_point))
+
+    })
+
+  }
+  
+  shinyApp(ui, server)
 }
