0edfd4114fe74b76db33c81b08d7c29a988952ca
Martin Petr <git@bodkan.net>
2021-05-23 22:32:18 +0200

    Add draft of time checks

--- R/utils.R
+++ R/utils.R
@@ -100,6 +100,73 @@ copy_attributes <- function(to, from, which) {
 }
 
 
+#' Get split times of populations in the lineage of the given population
+get_lineage_splits <- function(x) {
+  parent <- attr(x, "parent")
+  if (is.character(parent))
+    return(x$time[1])
+  else
+    return(c(x$time[1], get_lineage_splits(parent)))
+}
+
+
+#' Get direction of time implied by the model
+get_time_direction <- function(pop) {
+  times <- get_lineage_splits(pop)
+  if (length(times) == 1)
+    "unknown"
+  else if (all(diff(times) > 0))
+    "backward"
+  else
+    "forward"
+}
+
+
+#' Check the consistency of the given split time to the parent population
+check_split_time <- function(time, parent) {
+  parent_time <- parent$time[1]
+  direction <- get_time_direction(parent)
+  if (direction == "forward" & time <= parent_time) {
+    stop(sprintf("The model implies forward time direction but the specified split time (%d) is lower than the parent's (%s)",
+                 time, parent_time),
+         call. = FALSE)
+  } else if (direction == "backward" & time >= parent_time) {
+    stop(sprintf("The model implies backward time direction but the specified split time (%s) is higher than the parent's (%s)",
+                 time, parent_time),
+         call. = FALSE)
+  }
+}
+
+
+check_event_time <- function(time, pop) {
+  direction <- get_time_direction(pop)
+
+  previous_time <- pop$time[1]
+  if (direction == "forward" & any(time < previous_time)) {
+    stop(sprintf("The model implies forward time direction but the specified event time (%d) is lower than last active time (%s). ",
+                 time, previous_time),
+         call. = FALSE)
+  }
+  if (direction == "backward" & any(time > previous_time)) {
+    stop(sprintf("The model implies backward time direction but the specified event time (%d) is lower than last active time (%s). ",
+                 time, previous_time),
+         call. = FALSE)
+  }
+
+  removal_time <- attr(pop, "remove")
+  if (direction == "forward" & any(time > removal_time)) {
+    stop(sprintf("The specified event time (%d) is not consistent with the scheduled removal of %s (%s). ",
+                 time, pop$pop[1], removal_time),
+         call. = FALSE)
+  }
+  if (direction == "backward" & any(time < removal_time)) {
+    stop(sprintf("The specified event time (%d) is not consistent with the scheduled removal of %s (%s). ",
+                 time, pop$pop[1], removal_time),
+         call. = FALSE)
+  }
+}
+
+
 #' Pipe operator
 #'
 #' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
