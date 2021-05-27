bd580cd33ebaeba830b701d6ee026a33f4dc882b
Martin Petr <git@bodkan.net>
2021-05-27 17:38:46 +0200

    Check for the presence of the slim binary

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(libname, pkgname) {
+  # check for presence of the slim binary in user's PATH and display
+  # a warning if it's not present
+  path_check <- all(Sys.which("slimqwe") != "")
+  if (!path_check) {
+    packageStartupMessage(
+      "The slim binary was not found in your $PATH variable. Most of\n",
+      "the functionality in this package will work without any issues\n",
+      "but you will not be able to simulate data with the `slim()` function.\n",
+      "\nMake sure to modify the $PATH variable in your .Renviron file so \n",
+      "that it points to the directory containing the slim program.")
+  }
+}
