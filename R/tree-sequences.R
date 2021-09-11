eb71f5b69deff07f9834859320dde560f9f546bb
Martin Petr <git@bodkan.net>
2021-09-11 23:19:55 +0200

    Fix accessing slendr metadata

--- R/tree-sequences.R
+++ R/tree-sequences.R
@@ -1163,7 +1163,7 @@ get_table_data <- function(ts, model, spatial, simplify_to = NULL) {
 }
 
 get_sampling <- function(ts) {
-  ts$metadata$SLiM$user_metadata$slendr$sampling %>%
+  ts$metadata$SLiM$user_metadata$slendr[[1]]$sampling %>%
     purrr::transpose() %>%
     dplyr::as_tibble() %>%
     tidyr::unnest(cols = c("n", "pop", "time", "time_gen", "time_orig")) %>%
