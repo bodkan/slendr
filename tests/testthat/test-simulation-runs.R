ad64b5cdb2e5d0393c5c7cc377350520f29691d1
Martin Petr <git@bodkan.net>
2021-09-17 19:12:15 +0200

    Add option to open test script in SLiMgui

--- tests/testthat/test-simulation-runs.R
+++ tests/testthat/test-simulation-runs.R
@@ -1,10 +1,10 @@
 # Run a given forward/backward simulation with different combinations
 # of start-end and burnin parameters
-run_sim <- function(direction, start, burnin, gen_time, sim_length = NULL) {
+run_sim <- function(direction, start, burnin, gen_time, sim_length = NULL, method = "batch") {
   map <- world(xrange = c(0, 10), yrange = c(0, 10), landscape = "blank")
   p <- population("pop", N = 5, time = start, map = map, center = c(5, 5), radius = 1)
   model <- compile(p, direction = direction, sim_length = sim_length, dir = tempdir(), generation_time = gen_time, resolution = 1, competition_dist = 10, mate_dist = 10, dispersal_dist = 10, overwrite = TRUE)
-  slim(model, method = "batch", burnin = burnin, seq_length = 1, recomb_rate = 0, save_locations = TRUE, verbose = FALSE)
+  slim(model, burnin = burnin, seq_length = 1, recomb_rate = 0, save_locations = TRUE, verbose = FALSE, method = method)
 
   df <- suppressMessages(readr::read_tsv(file.path(model$path, "output_ind_locations.tsv.gz"),
                    progress = FALSE)) %>%
@@ -153,7 +153,6 @@ test_that("Backward simulation of limited length has the correct length with bur
   expect_true(min(result$time) == start - gen_time * round(sim_length / gen_time))
   expect_true(length(unique(result$time)) == round(sim_length / gen_time) + 1)
 })
-
 
 # backward simulations - generation time > 1 (times not multiples) --------
 
