e0f9a094f41585cab94b29629dcc36166b3e010c
Martin Petr <git@bodkan.net>
2021-10-18 13:46:15 +0200

    Cleanup the msprime script

--- inst/extdata/script.py
+++ inst/extdata/script.py
@@ -12,18 +12,24 @@ import pandas
 
 VERSION = "__VERSION__"
 
-parser = argparse.ArgumentParser("msprime script for executing slendr models")
+parser = argparse.ArgumentParser(
+    "msprime script for executing non-spatial slendr models"
+)
 parser.add_argument("--model", metavar="DIRECTORY", default=".",
-                   help="Location of a slendr model directory")
+                   help="Path to a slendr model directory")
 parser.add_argument("--output", metavar="FILE",
-                    help="Tree sequence output path")
+                    help="Path to a tree sequence output file")
 parser.add_argument("--sequence-length", required=True, type=float,
-                    help="Amount of sequence to simulate")
+                    help="The length of a sequence to simulate")
 parser.add_argument("--recombination-rate", required=True, type=float,
-                    help="Recombination rate")
+                    help="Uniform recombination rate")
 parser.add_argument("--sampling-schedule", metavar="FILE", required=True,
-                    help="Path to the table created by slendr's sampling()")
+                    help="Path to the slendr sampling schedule table "
+                         "(see the manpage of the `sampling()` function for "
+                         "more details)")
 parser.add_argument("--seed", type=int, help="Random seed value")
+parser.add_argument("--verbose", action="store_true", default=False,
+                    help="Print detailed logging information?")
 parser.add_argument("--debug", action="store_true", default=False,
                     help="Print a model debugging summary?")
 
@@ -33,7 +39,6 @@ if args.verbose:
     logging.basicConfig(level=logging.INFO)
 
 model_dir = os.path.expanduser(args.model)
-sampling_schedule = os.path.expanduser(args.sampling_schedule)
 
 if not args.output:
     args.output = pathlib.Path(model_dir, "output_msprime_ts.trees")
@@ -41,34 +46,39 @@ if not args.output:
 if not os.path.exists(model_dir):
     sys.exit(f"Model directory {model_dir} does not exist")
 
-model = {
-    "populations" : pathlib.Path(model_dir, "populations.tsv"),
-    "resizes"     : pathlib.Path(model_dir, "resizes.tsv"),
-    "geneflows"   : pathlib.Path(model_dir, "geneflow.tsv"),
-    "length"      : pathlib.Path(model_dir, "length.txt")
-}
+# paths to mandatory slendr configuration files
+populations_path = pathlib.Path(model_dir, "populations.tsv")
+resizes_path = pathlib.Path(model_dir, "resizes.tsv")
+geneflows_path = pathlib.Path(model_dir, "geneflow.tsv")
+length_path = pathlib.Path(model_dir, "length.txt")
+sampling_path = os.path.expanduser(args.sampling_schedule)
 
 # read model configuration files
-populations = pandas.read_table(model["populations"])
-resizes = pandas.read_table(model["resizes"]) if os.path.exists(model["resizes"]) else None
-# Python doesn't like the name of the column "from"
-if os.path.exists(model["geneflows"]):
-    geneflows = pandas.read_table(model["geneflows"]) \
+populations = pandas.read_table(populations_path)
+resizes = pandas.DataFrame()
+geneflows = pandas.DataFrame()
+
+if os.path.exists(resizes_path):
+    resizes = pandas.read_table(resizes_path)
+if os.path.exists(geneflows_path):
+    geneflows = pandas.read_table(geneflows_path) \
         .rename(columns={"from" : "source"})
-else:
-    geneflows = None
-length = int(float(open(model["length"], "r").readline().rstrip()))
+
+length = int(float(open(length_path, "r").readline().rstrip()))
 
 # read the sampling schedule table or create a default one
-if os.path.exists(sampling_schedule):
-    samples_df = pandas.read_table(sampling_schedule)
+if os.path.exists(sampling_path):
+    samples_df = pandas.read_table(sampling_path)
 else:
-    sys.exit(f"Sampling schedule table is missing")
+    sys.exit(f"Sampling schedule table at '{sampling_path}' does not exist")
 
 logging.info("Setting up an msprime demographic model")
 
-samples = [msprime.SampleSet(n, population=pop, time=length - time_gen + 1, ploidy=2)
-                             for (_, pop, n, time_gen, _) in samples_df.itertuples()]
+samples = [
+    msprime.SampleSet(
+        n, population=pop, time=length - time_gen + 1, ploidy=2
+    ) for (_, pop, n, time_gen, _) in samples_df.itertuples()
+]
 
 # set up demographic history
 demography = msprime.Demography()
@@ -82,16 +92,30 @@ for pop in populations.itertuples():
             ancestral=pop.parent
         )
 
-# set gene flow events
+# # schedule population size changes
+# for event in resizes.itertuples():
+#     if event.how == "step":
+#         demography.add_population_parameters_change(
+#             time=length - event.tresize
+#         )
+#     else:
+#         demography.add_population_parameters_change(
+#             time=length - event.tresize
+#         )
+
+# resize_table[, c("pop", "pop_id", "how", "N", "prev_N",
+#                    "tresize_orig", "tresize_gen", "tend_orig", "tend_gen")]
+
+# schedule gene flow events
 for event in geneflows.itertuples():
     demography.add_migration_rate_change(
-        time=length - event.tstart_gen + 1,
+        time=length - event.tend_gen + 1,
         rate=event.rate,
         source=event.to,
         dest=event.source,
     )
     demography.add_migration_rate_change(
-        time=length - event.tend_gen + 1,
+        time=length - event.tstart_gen + 1,
         rate=0,
         source=event.to,
         dest=event.source,
