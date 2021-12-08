0b1f7917b87fd35cef5828914c2f6f795c72b9ce
Martin Petr <git@bodkan.net>
2021-12-08 13:20:55 +0100

    Add unit test for multiple spatial sampling points

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -66,7 +66,7 @@ test_that("only locations within world bounds are valid", {
   expect_silent(check_location_bounds(valid, map))
 })
 
-test_that("sampling takes place as close to the specified position as possible", {
+test_that("sampling is as close to the a single specified position as possible", {
   skip_if(!env_present("retipy"))
 
   n_samples <- 5
@@ -120,4 +120,91 @@ test_that("sampling takes place as close to the specified position as possible",
   expect_true(all.equal(individuals$x, locations$x, tolerance = 0.001))
   expect_true(all.equal(individuals$y, locations$y, tolerance = 0.001))
   expect_true(all.equal(individuals$distance, locations$distance, tolerance = 0.001))
+})
+
+test_that("sampling is as close to the multiple specified positions as possible", {
+  skip_if(!env_present("retipy"))
+
+  n_samples <- 5
+  times <- c(10, 100)
+  locations <- list(c(0, 0), c(100, 100))
+  sim_length <- 100
+
+  map <- world(xrange = c(0, 100), yrange = c(0, 100), landscape = "blank")
+  pop <- population("pop", 1, 100, map = map, center = c(50, 50), radius = 50)
+  model <- compile(pop, dir = tempdir(), generation_time = 1,
+                   competition_dist = 10, mate_dist = 10, dispersal_dist = 10,
+                   sim_length = sim_length, resolution = 1, overwrite = TRUE)
+
+  samples <- rbind(
+    sampling(model, times = times[1], locations = locations[1], list(pop, n_samples)),
+    sampling(model, times = times[2], locations = locations[2], list(pop, n_samples))
+  )
+
+  slim(model, sequence_length = 1, recombination_rate = 0, sampling = samples,
+       method = "batch", save_locations = TRUE, verbose = FALSE)
+
+  # load the locations of all individuals throughout the simulation, and filter
+  # down to `n_sample` of the ones closest to the sampling location [50, 50] in
+  # each time point (essentially replicating what we're doing on the SLiM
+  # backend during the simulation run)
+  all_locations <- file.path(model$path, "output_ind_locations.tsv.gz") %>%
+    readr::read_tsv(show_col_types = FALSE) %>%
+    dplyr::mutate(time = sim_length - gen + 1)
+
+  first_loc <- all_locations %>%
+    dplyr::filter(time == times[1]) %>%
+    dplyr::group_by(time) %>%
+    dplyr::mutate(distance = sqrt((locations[[1]][1] - x)^2 + (locations[[1]][2] - y)^2)) %>%
+    dplyr::arrange(time, distance) %>%
+    dplyr::mutate(i = 1:dplyr::n()) %>%
+    dplyr::filter(i %in% 1:n_samples) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(ind, time, x, y, distance) %>%
+    dplyr::arrange(time, distance) %>%
+    as.data.frame()
+
+  second_loc <- all_locations %>%
+    dplyr::filter(time == times[2]) %>%
+    dplyr::group_by(time) %>%
+    dplyr::mutate(distance = sqrt((locations[[2]][1] - x)^2 + (locations[[2]][2] - y)^2)) %>%
+    dplyr::arrange(time, distance) %>%
+    dplyr::mutate(i = 1:dplyr::n()) %>%
+    dplyr::filter(i %in% 1:n_samples) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(ind, time, x, y, distance) %>%
+    dplyr::arrange(time, distance) %>%
+    as.data.frame()
+
+  locs <- rbind(first_loc, second_loc)
+
+  # load locations of individuals remembered in the tree sequence
+  ts <- ts_load(model)
+  all_individuals <- ts_data(ts, remembered = TRUE) %>%
+    dplyr::select(-node_id) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(
+      x = sf::st_coordinates(.)[, "X"],
+      y = sf::st_coordinates(.)[, "Y"]
+    ) %>%
+    dplyr::as_tibble() %>%
+    dplyr::select(ind_id, time, x, y)
+
+  first_individuals <- all_individuals %>%
+    dplyr::filter(time == times[1]) %>%
+    dplyr::mutate(distance = sqrt((locations[[1]][1] - x)^2 + (locations[[1]][2] - y)^2)) %>%
+    dplyr::arrange(time, distance) %>%
+    as.data.frame()
+
+  second_individuals <- all_individuals %>%
+    dplyr::filter(time == times[2]) %>%
+    dplyr::mutate(distance = sqrt((locations[[2]][1] - x)^2 + (locations[[2]][2] - y)^2)) %>%
+    dplyr::arrange(time, distance) %>%
+    as.data.frame()
+
+  individuals <- rbind(first_individuals, second_individuals)
+
+  expect_true(all.equal(individuals$x, locs$x, tolerance = 0.001))
+  expect_true(all.equal(individuals$y, locs$y, tolerance = 0.001))
+  expect_true(all.equal(individuals$distance, locs$distance, tolerance = 0.001))
 })
