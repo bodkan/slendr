6b18d0c6d208e334bc1b1587eba3c93940a929f4
Martin Petr <git@bodkan.net>
2022-01-11 11:21:56 +0100

    Add first tests of population size locking

---
+++ tests/testthat/test-resizes-locked.R
@@ -0,0 +1,45 @@
+map <- world(xrange = c(0, 1e6), yrange = c(0, 1e6), landscape = "blank")
+
+test_that("population size is correctly decreased upon reducing circular boundary", {
+  r1 <- 300e3
+  r2 <- 100e3
+  N1 <- 100
+
+  pop <- population("pop", time = 1, N = N1, map = map, center = c(500, 500),
+                    radius = r1) |>
+    boundary(time = 500, center = c(500, 500), radius = r2, lock = TRUE)
+
+  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+
+  # make sure that the new population size corresponds to the decrease in the
+  # circular area of the population boundary
+  area1 <- pi * r1^2
+  area2 <- pi * r2^2
+
+  N2 <- round(N1 * area2 / area1)
+
+  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+})
+
+test_that("population size is correctly increased upon exanding circular baoundary", {
+  r1 <- 100e3
+  r2 <- 300e3
+  N1 <- 100
+
+  pop <- population("pop", time = 1, N = N1, map = map, center = c(500, 500),
+                    radius = r1) |>
+    boundary(time = 500, center = c(500, 500), radius = r2, lock = TRUE)
+
+  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+
+  # make sure that the new population size corresponds to the decrease in the
+  # circular area of the population boundary
+  area1 <- pi * r1^2
+  area2 <- pi * r2^2
+
+  N2 <- round(N1 * area2 / area1)
+
+  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+})
