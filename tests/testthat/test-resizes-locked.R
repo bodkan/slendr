4a5e2cddea0cb114b75fff85f7e1d55840cff880
Martin Petr <git@bodkan.net>
2022-01-11 11:52:53 +0100

    Shorten the new unit test simulations

--- tests/testthat/test-resizes-locked.R
+++ tests/testthat/test-resizes-locked.R
@@ -7,9 +7,9 @@ test_that("population size is correctly decreased upon reducing circular boundar
 
   pop <- population("pop", time = 1, N = N1, map = map, center = c(500, 500),
                     radius = r1) |>
-    boundary(time = 500, center = c(500, 500), radius = r2, lock = TRUE)
+    boundary(time = 50, center = c(500, 500), radius = r2, lock = TRUE)
 
-  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+  ind_df <- run_sim(pop, "forward", sim_length = 55, verbose = FALSE)
 
   # make sure that the new population size corresponds to the decrease in the
   # circular area of the population boundary
@@ -18,8 +18,8 @@ test_that("population size is correctly decreased upon reducing circular boundar
 
   N2 <- round(N1 * area2 / area1)
 
-  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
-  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+  expect_true(all(ind_df[ind_df$time < 50, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 50, "N"] == N2))
 })
 
 test_that("population size is correctly increased upon exanding circular baoundary", {
@@ -29,9 +29,9 @@ test_that("population size is correctly increased upon exanding circular baounda
 
   pop <- population("pop", time = 1, N = N1, map = map, center = c(500, 500),
                     radius = r1) |>
-    boundary(time = 500, center = c(500, 500), radius = r2, lock = TRUE)
+    boundary(time = 50, center = c(500, 500), radius = r2, lock = TRUE)
 
-  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+  ind_df <- run_sim(pop, "forward", sim_length = 55, verbose = FALSE)
 
   # make sure that the new population size corresponds to the decrease in the
   # circular area of the population boundary
@@ -40,8 +40,8 @@ test_that("population size is correctly increased upon exanding circular baounda
 
   N2 <- round(N1 * area2 / area1)
 
-  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
-  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+  expect_true(all(ind_df[ind_df$time < 50, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 50, "N"] == N2))
 })
 
 test_that("population size is correctly decreased upon reducing square boundary", {
@@ -51,9 +51,9 @@ test_that("population size is correctly decreased upon reducing square boundary"
 
   pop <- population("pop", time = 1, N = N1, map = map,
                     polygon = list(c(0, 0), c(a1, 0), c(a1, a1), c(0, a1))) |>
-    boundary(time = 500, lock = TRUE, polygon = list(c(0, 0), c(a2, 0), c(a2, a2), c(0, a2)))
+    boundary(time = 50, lock = TRUE, polygon = list(c(0, 0), c(a2, 0), c(a2, a2), c(0, a2)))
 
-  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+  ind_df <- run_sim(pop, "forward", sim_length = 55, verbose = FALSE)
 
   # make sure that the new population size corresponds to the decrease in the
   # circular area of the population boundary
@@ -62,8 +62,8 @@ test_that("population size is correctly decreased upon reducing square boundary"
 
   N2 <- round(N1 * area2 / area1)
 
-  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
-  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+  expect_true(all(ind_df[ind_df$time < 50, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 50, "N"] == N2))
 })
 
 
@@ -74,9 +74,9 @@ test_that("population size is correctly decreased upon expanding square boundary
 
   pop <- population("pop", time = 1, N = N1, map = map,
                     polygon = list(c(0, 0), c(a1, 0), c(a1, a1), c(0, a1))) |>
-    boundary(time = 500, lock = TRUE, polygon = list(c(0, 0), c(a2, 0), c(a2, a2), c(0, a2)))
+    boundary(time = 50, lock = TRUE, polygon = list(c(0, 0), c(a2, 0), c(a2, a2), c(0, a2)))
 
-  ind_df <- run_sim(pop, "forward", sim_length = 550, verbose = FALSE)
+  ind_df <- run_sim(pop, "forward", sim_length = 55, verbose = FALSE)
 
   # make sure that the new population size corresponds to the decrease in the
   # circular area of the population boundary
@@ -85,6 +85,6 @@ test_that("population size is correctly decreased upon expanding square boundary
 
   N2 <- round(N1 * area2 / area1)
 
-  expect_true(all(ind_df[ind_df$time < 500, "N"] == N1))
-  expect_true(all(ind_df[ind_df$time >= 500, "N"] == N2))
+  expect_true(all(ind_df[ind_df$time < 50, "N"] == N1))
+  expect_true(all(ind_df[ind_df$time >= 50, "N"] == N2))
 })
