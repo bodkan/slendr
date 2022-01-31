1af33258651555efecc84b86ac259b40b4c22779
Martin Petr <git@bodkan.net>
2022-01-31 22:48:12 +0100

    Update manpages

--- man/examples/tree_sequences.R
+++
@@ -1,9 +0,0 @@
-
-
-
-
-ts_tajima(ts, list(afr = c("AFR_1", "AFR_2", "AFR_3"), eur = c("EUR_1", "EUR_2")))
-
-ts_diversity(ts, sample_sets) %>% dplyr::arrange(diversity)
-
-ts_divergence(ts, sample_sets) %>% arrange(divergence)
