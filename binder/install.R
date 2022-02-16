49bdf886591da76e65f2541f16af49a4a594a7a9
Martin Petr <git@bodkan.net>
2022-02-16 12:36:23 +0100

    Update binder pipeline

--- binder/install.R
+++ binder/install.R
@@ -1,1 +1,5 @@
 devtools::install_github("bodkan/slendr") # 2022-02-16 12:12
+
+# create the Miniconda environment so that users don't have to wait
+# for setup_env() in the vignette examples
+R -e 'slendr::setup_env()'
