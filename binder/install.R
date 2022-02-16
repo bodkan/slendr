d90bd2e08b3a7911475cbcf1f1a74bd909103d6f
Martin Petr <git@bodkan.net>
2022-02-16 15:25:32 +0100

    Update reticulate env creation

--- binder/install.R
+++ binder/install.R
@@ -4,4 +4,7 @@ devtools::install_github("bodkan/slendr")
 
 # create the Miniconda environment so that users don't have to wait
 # for setup_env() in the vignette examples
-slendr::setup_env()
+reticulate::conda_create(
+  packages = c("msprime=1.1.0", "tskit=0.4.1", "pyslim=0.700", "pandas=1.3.5"),
+  envname = "automatic_slendr_python_env"
+)
