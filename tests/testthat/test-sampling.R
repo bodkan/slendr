60102dfb2fcc305d07f7df9076349e005e4192e4
Martin Petr <git@bodkan.net>
2022-05-08 17:50:01 +0200

    Rename 'sampled' to 'focal'

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -105,7 +105,7 @@ test_that("sampling is as close to the a single specified position as possible",
   # load locations of individuals remembered in the tree sequence
   ts <- ts_load(model)
   individuals <- ts_nodes(ts) %>%
-    dplyr::filter(sampled) %>%
+    dplyr::filter(focal) %>%
     dplyr::select(-node_id) %>%
     dplyr::distinct() %>%
     dplyr::mutate(
@@ -182,7 +182,7 @@ test_that("sampling is as close to the multiple specified positions as possible"
   # load locations of individuals remembered in the tree sequence
   ts <- ts_load(model)
   all_individuals <- ts_nodes(ts) %>%
-    dplyr::filter(sampled) %>%
+    dplyr::filter(focal) %>%
     dplyr::select(-node_id) %>%
     dplyr::distinct() %>%
     dplyr::mutate(
