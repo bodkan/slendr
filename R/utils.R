30e2a323d0185f6dd947a617ce4b5b4ecb6589e2
Martin Petr <git@bodkan.net>
2022-07-15 15:36:40 +0200

    Update example

--- R/utils.R
+++ R/utils.R
@@ -14,7 +14,6 @@
 #' # load an example model with an already simulated tree sequence
 #' model <- read_example("introgression")
 #' plot_model(model)
-#'
 #' @export
 read_example <- function(name = c("introgression", "space")) {
   name <- match.arg(name)
