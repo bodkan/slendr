3d64b899a2042ab84f941da90cafb86d0764a652
Martin Petr <git@bodkan.net>
2022-09-05 22:57:37 +0200

    Fix broken test of the new functionality

--- tests/testthat/test-nonserialized.R
+++ tests/testthat/test-nonserialized.R
@@ -7,14 +7,11 @@ eur <- population("EUR", parent = ehg, time = 25000, N = 200) %>%
   resize(N = 10000, how = "exponential", time = 5000, end = 0)
 ana <- population("ANA", time = 28000, N = 300, parent = ooa, remove = 40)
 
-gf <- list(
-  gene_flow(from = ana, to = yam, rate = 0.5, start = 6500, end = 6400),
-  gene_flow(from = ana, to = eur, rate = 0.5, start = 8000, end = 6000)
-)
+gf <- gene_flow(from = ana, to = eur, rate = 0.5, start = 8000, end = 6000)
 
 test_that("non-serialized models are correctly simulated with msprime (but not SLiM)", {
   model <- compile_model(
-    populations = list(ooa, ehg, eur, ana, yam),
+    populations = list(ooa, ehg, eur, ana),
     gene_flow = gf, generation_time = 30,
     serialize = FALSE
   )
@@ -33,7 +30,7 @@ test_that("non-serialized models are correctly simulated with msprime (but not S
 test_that("non-serialized models give the same result as serialized models", {
   # create a serialized model
   model_ser <- compile_model(
-    populations = list(ooa, ehg, eur, ana, yam),
+    populations = list(ooa, ehg, eur, ana),
     gene_flow = gf, generation_time = 30
   )
   expect_true(file.exists(model_ser$path))
@@ -41,7 +38,7 @@ test_that("non-serialized models give the same result as serialized models", {
 
   # compile a non-serialized model
   model_nonser <- compile_model(
-    populations = list(ooa, ehg, eur, ana, yam),
+    populations = list(ooa, ehg, eur, ana),
     gene_flow = gf, generation_time = 30,
     serialize = FALSE
   )
@@ -52,12 +49,12 @@ test_that("non-serialized models give the same result as serialized models", {
                "not possible to simulate non-serialized models")
   expect_s3_class(slim(model_ser, sequence_length = 1, recombination_rate = 0), "slendr_ts")
 
-  ts_nonser <- msprime(model_nonser, sequence_length = 10000, recombination_rate = 0) %>%
+  ts_nonser <- msprime(model_nonser, sequence_length = 10000, recombination_rate = 0, random_seed = 42) %>%
     ts_mutate(mutation_rate = 0.0001, random_seed = 42)
   expect_s3_class(ts_nonser, "slendr_ts")
   expect_s3_class(ts_nonser, "tskit.trees.TreeSequence")
 
-  ts_ser <- msprime(model_nonser, sequence_length = 10000, recombination_rate = 0) %>%
+  ts_ser <- msprime(model_ser, sequence_length = 10000, recombination_rate = 0, random_seed = 42) %>%
     ts_mutate(mutation_rate = 0.0001, random_seed = 42)
   expect_s3_class(ts_ser, "slendr_ts")
   expect_s3_class(ts_ser, "tskit.trees.TreeSequence")
@@ -68,19 +65,21 @@ test_that("non-serialized models give the same result as serialized models", {
 
   # check equivalence of raw tree-sequence tables
   expect_true(all(ts_table(ts_ser, "individuals") == ts_table(ts_nonser, "individuals"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser, "edges") == ts_table(ts_nonser, "edges"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser, "nodes") == ts_table(ts_nonser, "nodes"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser, "mutations") == ts_table(ts_nonser, "mutations"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser, "edges")       == ts_table(ts_nonser, "edges"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser, "nodes")       == ts_table(ts_nonser, "nodes"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser, "mutations")   == ts_table(ts_nonser, "mutations"), na.rm = TRUE))
 
   # check equivalence of simplification for both tree sequences
-  ts_ser_small <- ts_simplify(ts_ser, simplify_to = c("EUR_1", "ANA_1", "EHG_1"))
+  ts_ser_small    <- ts_simplify(ts_ser,    simplify_to = c("EUR_1", "ANA_1", "EHG_1"))
   ts_nonser_small <- ts_simplify(ts_nonser, simplify_to = c("EUR_1", "ANA_1", "EHG_1"))
+
   expect_true(all(ts_nodes(ts_ser_small) == ts_nodes(ts_nonser_small), na.rm = TRUE))
   expect_true(all(ts_edges(ts_ser_small) == ts_edges(ts_nonser_small), na.rm = TRUE))
+
   expect_true(all(ts_table(ts_ser_small, "individuals") == ts_table(ts_nonser_small, "individuals"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser_small, "edges") == ts_table(ts_nonser_small, "edges"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser_small, "nodes") == ts_table(ts_nonser_small, "nodes"), na.rm = TRUE))
-  expect_true(all(ts_table(ts_ser_small, "mutations") == ts_table(ts_nonser_small, "mutations"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser_small, "edges")       == ts_table(ts_nonser_small, "edges"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser_small, "nodes")       == ts_table(ts_nonser_small, "nodes"), na.rm = TRUE))
+  expect_true(all(ts_table(ts_ser_small, "mutations")   == ts_table(ts_nonser_small, "mutations"), na.rm = TRUE))
 
   # check equivalence of trees of both tree sequences
   tree_ser <- ts_phylo(ts_ser_small, 1, quiet = TRUE)
