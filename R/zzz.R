c0e497c607e1f054c9b442cbbf57c6bba7df1dd6
Martin Petr <git@bodkan.net>
2023-02-01 14:01:02 +0100

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -53,13 +53,13 @@ PYTHON_ENV <-
 
   packageStartupMessage(
     "=======================================================================\n",
-    "NOTE: Due to issues with Python setups on some systems which have been\n",
+    "NOTE: Due to Python setup issues on some systems which have been\n",
     "causing trouble particularly for novice users, calling library(slendr)\n",
     "no longer activates slendr's Python environment automatically.\n\n",
     "In order to use slendr's msprime back end or its tree-sequence\n",
     "functionality, users must now activate slendr's Python environment\n",
     "manually by executing init_env() after calling library(slendr).\n\n",
-    "(This note will be removed in a future version of slendr.)",
+    "(This note will be removed in the next major version of slendr.)",
     "\n======================================================================="
   )
 }
