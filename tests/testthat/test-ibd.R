2e8854d0c83ab6c3e6b4bef316b29f5a78e3341a
Martin Petr <git@bodkan.net>
2023-02-28 21:35:03 +0100

    Add a first unit test

--- tests/testthat/test-ibd.R
+++ tests/testthat/test-ibd.R
@@ -1,10 +1,22 @@
+devtools::load_all()
 skip_if(!is_slendr_env_present())
+init_env()
 
 pop <- population("POP", time = 1, N = 1000)
 model <- compile_model(populations = pop, generation_time = 1, simulation_length = 1000)
 
 ts <- msprime(model, sequence_length = 10e6, recombination_rate = 1e-8)
 
-test_that("ts_ibd() works", {
-  expect_equal(2 * 2, 4)
+test_that("aggregate ts_ibd(ts, coordinates = TRUE) matches IBD totals", {
+  ibd_totals <- ts_ibd(ts, coordinates = FALSE, min_length = 5e6)
+  ibd_fragments <- ts_ibd(ts, coordinates = TRUE, min_length = 5e6)
+
+  # compute IBD totals from individual fragments manually
+  ibd_totals2 <-
+    dplyr::group_by(ibd_fragments, node1, node2, name1, name2, pop1, pop2) %>%
+    dplyr::summarise(count = dplyr::n(), total = sum(length), .groups = "keep") %>%
+    dplyr::select(count, total, dplyr::everything()) %>%
+    dplyr::ungroup()
+
+  expect_equal(ibd_totals, ibd_totals2)
 })
