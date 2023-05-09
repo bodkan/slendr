140b67e56d1c31829ca6e99124909ac4f58c7fda
Martin Petr <git@bodkan.net>
2023-05-09 19:07:46 +0200

    Add condition to unit test

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -225,6 +225,8 @@ test_that("sampling locations may only be given for spatial models", {
 })
 
 test_that("a mix of spatial and non-spatial samplings is not allowed for a single population", {
+  skip_if(!is_slendr_env_present())
+
   map <- world(xrange = c(0, 100), yrange = c(0, 100), landscape = "blank")
   p1 <- population(name = "p1", time = 10, N = 100)
   p2 <- population(name = "p2", map = map, time = 20, N = 100, center = c(1, 1), radius = 10)
@@ -250,7 +252,6 @@ test_that("a mix of spatial and non-spatial samplings is not allowed for a singl
 
 test_that("sampling table is correctly adjusted after simplification (msprime)", {
   skip_if(!is_slendr_env_present())
-  init_env(quiet = TRUE)
 
   pop1 <- population("pop1", time = 1, N = 100)
   pop2 <- population("pop2", time = 10, N = 42, parent = pop1)
@@ -284,7 +285,6 @@ test_that("sampling table is correctly adjusted after simplification (msprime)",
 
 test_that("sampling table is correctly adjusted after simplification (SLiM)", {
   skip_if(!is_slendr_env_present())
-  init_env(quiet = TRUE)
 
   pop1 <- population("pop1", time = 1, N = 100)
   pop2 <- population("pop2", time = 10, N = 42, parent = pop1)
