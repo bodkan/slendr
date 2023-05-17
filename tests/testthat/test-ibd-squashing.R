6ca4d17293f260111078bf16c90854e782cc8a24
Martin Petr <git@bodkan.net>
2023-05-17 16:02:09 +0200

    Expand squashing unit tests

--- tests/testthat/test-ibd-squashing.R
+++ tests/testthat/test-ibd-squashing.R
@@ -1,4 +1,7 @@
 skip_if(!is_slendr_env_present())
+init_env(quiet = TRUE)
+
+reticulate::py_run_string("import warnings; import msprime; warnings.simplefilter('ignore', msprime.TimeUnitsMismatchWarning)")
 
 #
 # first batch of tests involves a small, manually constructed tree sequence below
@@ -241,13 +244,11 @@ test_that("`squash = TRUE` with `minimum_length` cutoff gives a warning", {
 # second batch of tests involves working with a slim tree sequence
 #
 
-ts_slim <- population("pop", N = 1000, time = 1) %>%
-  compile_model(generation_time = 1, simulation_length = 1000) %>%
-  slim(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42, coalescent_only = FALSE) %>%
-  ts_recapitate(Ne = 1000, recombination_rate = 1e-8, random_seed = 42) %>%
-  ts_simplify(simplify_to = paste0("pop_", 1:5), filter_nodes = FALSE, keep_unary = TRUE)
+test_that("tree sequence with unary nodes produces correct squashing results (SLiM raw)", {
+  ts_slim <- population("pop", N = 10, time = 1) %>%
+    compile_model(generation_time = 1, simulation_length = 1000) %>%
+    slim(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42, coalescent_only = FALSE)
 
-test_that("full tree sequence with unary nodes produces correct squashing results (SLiM)", {
   # get IBDs from a tree sequence without and with squashing
   ibd <- ts_ibd(ts_slim, coordinates = TRUE, minimum_length = 0)
   suppressWarnings(ibd_squashed <- ts_ibd(ts_slim, coordinates = TRUE, squash = TRUE))
@@ -260,15 +261,71 @@ test_that("full tree sequence with unary nodes produces correct squashing result
 })
 
 
+test_that("tree sequence with unary nodes produces correct squashing results (SLiM recapitated)", {
+  ts_slim <- population("pop", N = 10, time = 1) %>%
+    compile_model(generation_time = 1, simulation_length = 1000) %>%
+    slim(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42, coalescent_only = FALSE) %>%
+    ts_recapitate(Ne = 1000, recombination_rate = 1e-8, random_seed = 42)
+
+  # get IBDs from a tree sequence without and with squashing
+  ibd <- ts_ibd(ts_slim, coordinates = TRUE, minimum_length = 0)
+  suppressWarnings(ibd_squashed <- ts_ibd(ts_slim, coordinates = TRUE, squash = TRUE))
+
+  # on the non-squashed IBD data frame, perform the squashing "manually"
+  ibd_summary <- manual_squash(ibd)
+
+  # the result must be equivalent to the squashing result done on the Python/tskit level
+  expect_true(all(ibd_summary == ibd_squashed))
+})
+
+test_that("tree sequence with unary nodes produces correct squashing results (SLiM unary simplified)", {
+  ts_slim <- population("pop", N = 1000, time = 1) %>%
+    compile_model(generation_time = 1, simulation_length = 1000) %>%
+    slim(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42, coalescent_only = FALSE) %>%
+    ts_recapitate(Ne = 1000, recombination_rate = 1e-8, random_seed = 42) %>%
+    ts_simplify(paste0("pop_", 1:10), filter_nodes = FALSE, keep_unary = TRUE)
+
+  # get IBDs from a tree sequence without and with squashing
+  ibd <- ts_ibd(ts_slim, coordinates = TRUE, minimum_length = 0)
+  suppressWarnings(ibd_squashed <- ts_ibd(ts_slim, coordinates = TRUE, squash = TRUE))
+
+  # on the non-squashed IBD data frame, perform the squashing "manually"
+  ibd_summary <- manual_squash(ibd)
+
+  # the result must be equivalent to the squashing result done on the Python/tskit level
+  expect_true(all(ibd_summary == ibd_squashed))
+})
+
+test_that("tree sequence with unary nodes produces correct squashing results (SLiM no-unary simplified)", {
+  ts_slim <- population("pop", N = 1000, time = 1) %>%
+    compile_model(generation_time = 1, simulation_length = 1000) %>%
+    slim(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42, coalescent_only = FALSE) %>%
+    ts_recapitate(Ne = 1000, recombination_rate = 1e-8, random_seed = 42) %>%
+    ts_simplify(paste0("pop_", 1:10))
+
+  # get IBDs from a tree sequence without and with squashing
+  ibd <- ts_ibd(ts_slim, coordinates = TRUE, minimum_length = 0)
+  suppressWarnings(ibd_squashed <- ts_ibd(ts_slim, coordinates = TRUE, squash = TRUE))
+
+  # on the non-squashed IBD data frame, perform the squashing "manually"
+  ibd_summary <- manual_squash(ibd)
+
+  # the result must be equivalent to the squashing result done on the Python/tskit level
+  expect_true(all(ibd_summary == ibd_squashed))
+})
+
+#
+# third batch of tests involves working with a slim tree sequence
+# TODO: check tests also with `filter_nodes = FALSE` (but see a bug below in ts_simplify())
+#
+
 ts_msprime <- population("pop", N = 1000, time = 1) %>%
   compile_model(generation_time = 1, simulation_length = 1000) %>%
   msprime(sequence_length = 1e6, recombination_rate = 1e-8, random_seed = 42)
 
-debugonce(get_tskit_table_data)
-ts_msprime %>%
-  ts_simplify(simplify_to = paste0("pop_", 1:5), filter_nodes = FALSE)
-ts_msprime %>%
-  ts_simplify(simplify_to = paste0("pop_", 1:5))
+# TODO: fix filter_nodes = FALSE for msprime tree sequences (branch `fix-msprime-filter-nodes`)
+# ts_msprime %>% ts_simplify(simplify_to = paste0("pop_", 1:5), filter_nodes = FALSE)
+ts_msprime <- ts_msprime %>% ts_simplify(simplify_to = paste0("pop_", 1:5))
 
 test_that("full tree sequence with unary nodes produces correct squashing results (SLiM)", {
   # get IBDs from a tree sequence without and with squashing
