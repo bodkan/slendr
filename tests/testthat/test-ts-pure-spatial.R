ee09a9d8af23ce8550a9cbbe564b2b40d06c90d8
Martin Petr <git@bodkan.net>
2024-01-25 01:13:04 -0800

    Normalize additional paths

--- tests/testthat/test-ts-pure-spatial.R
+++ tests/testthat/test-ts-pure-spatial.R
@@ -6,8 +6,8 @@ init_env(quiet = TRUE)
 set.seed(42)
 
 script_file <- tempfile()
-ts_file <- tempfile()
-loc_file <- tempfile()
+ts_file <- normalizePath(tempfile(), winslash = "/", mustWork = FALSE)
+loc_file <- normalizePath(tempfile(), winslash = "/", mustWork = FALSE)
 
 writeLines(sprintf('
 initialize() {
