aa002ad30ebc268ce0f242fd415c7f98c35da373
Martin Petr <git@bodkan.net>
2024-03-26 17:22:49 +0100

    Custom script blocks can execute after all slendr script blocks (closes #129)

--- R/engines.R
+++ R/engines.R
@@ -420,7 +420,7 @@ slim <- function(
               paste(log_warnings, collapse = "\n"), call. = FALSE)
     }
 
-    if (!grepl("simulation finished", log_output[length(log_output)])) {
+    if (!any(grepl("simulation finished", log_output))) {
       if (!verbose) cat(log_output, sep = "\n")
       stop("Unfortunately SLiM terminated before a tree sequence was saved.\n",
            "See the above for an indication of where things ",
