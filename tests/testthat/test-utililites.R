f3e1530c1fc9ee2f3b811e1566c7a7196733d097
Martin Petr <git@bodkan.net>
2025-02-25 09:20:44 +0100

    Add a get_env() unit test

--- tests/testthat/test-utililites.R
+++ tests/testthat/test-utililites.R
@@ -20,3 +20,9 @@ test_that("concat() function correctly collapses sample identifiers", {
   expect_equal(concat(list(c("A", "B"))), "A+B")
   expect_equal(concat(list(xyz=c("A", "B"))), "xyz")
 })
+
+test_that("get_env() returns the correct environment name", {
+  skip_if_not(check_dependencies(python = TRUE))
+  get_env() == PYTHON_ENV
+})
+
