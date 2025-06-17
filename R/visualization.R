9da2ba537352ddcd748844ae849a9f3ad7cfc450
Martin Petr <git@bodkan.net>
2025-06-17 20:51:01 +0200

    Fix issues with recently added split visualisation

--- R/visualization.R
+++ R/visualization.R
@@ -39,7 +39,7 @@ plot_map <- function(..., time = NULL, gene_flow = FALSE, splits = FALSE,
   args <- list(...)
 
   if (!all(sapply(args, inherits, "slendr")))
-    stop("Only objects of the class 'slendr' can be visualized using plot.slendr", call. = FALSE)
+    stop("Only objects of the class 'slendr' can be visualized using plot_map", call. = FALSE)
 
   classes <- grep("slendr_", unique(unlist(sapply(args, class))), value = TRUE)
   if (length(classes) > 1 & "slendr_model" %in% classes)
@@ -168,14 +168,14 @@ plot_map <- function(..., time = NULL, gene_flow = FALSE, splits = FALSE,
     if (splits) {
       # make a copy of a the sf object with initial population ranges
       # TODO: Instead find the temporally closest range for each given split event
-      split_lines <- first_ranges
-      centroids <- sf::st_centroid(split_lines$geometry)
+      split_lines <- st_set_agr(first_ranges, "constant")
+      centroids <- st_centroid(split_lines)
       # add end point
-      split_lines$to <- centroids
+      split_lines$to <- centroids$geometry
       # add starting point
       split_lines$parent_pop <- model$splits$parent[model$splits$pop %in% split_lines$pop]
       split_lines <- split_lines[split_lines$parent_pop != "ancestor", ]
-      split_lines$from <- centroids[split_lines$pop %in% split_lines$parent_pop]
+      split_lines$from <- do.call(rbind, lapply(split_lines$parent_pop, function(x) st_as_sf(centroids[centroids$pop == x, ])))$geometry
       # form a line
       split_lines$geometry <- Map(sf::st_union, split_lines$from, split_lines$to) %>%
         sf::st_as_sfc(.) %>% sf::st_cast("LINESTRING")
